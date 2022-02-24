52fcc58a369d7ce8ff38ed1af6158692e1e6a90f
barroodc <53627915+barroodc@users.noreply.github.com>
2022-02-23 23:22:50 -0500

    AbstractMySQLDao + IBaseDao

--- src/main/java/com/chrisportfolio/schoolmanagementsystem/dao/daointerfaces/IBaseDao.java
+++ src/main/java/com/chrisportfolio/schoolmanagementsystem/dao/daointerfaces/IBaseDao.java
@@ -1,4 +1,12 @@
 package com.chrisportfolio.schoolmanagementsystem.dao.daointerfaces;
 
-public interface IBaseDao {
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IBaseDao<T> {
+    T findByID(long id) throws SQLException;
+    List<T> findAll();
+    T update(T dto);
+    T create(T dto);
+    void delete(long id);
 }
--- src/main/java/com/chrisportfolio/schoolmanagementsystem/dao/jdbcmySQLImpl/AbstractMySQLDao.java
+++ src/main/java/com/chrisportfolio/schoolmanagementsystem/dao/jdbcmySQLImpl/AbstractMySQLDao.java
@@ -1,4 +1,17 @@
 package com.chrisportfolio.schoolmanagementsystem.dao.jdbcmySQLImpl;
 
-public class AbstractMySQLDao {
+import com.chrisportfolio.schoolmanagementsystem.dao.daointerfaces.IBaseDao;
+
+import java.sql.Connection;
+
+public abstract class AbstractMySQLDao<T> implements IBaseDao<T> {
+    protected Connection connection;
+
+    public AbstractMySQLDao(Connection connection) {
+        this.connection = connection;
+    }
+
+    public AbstractMySQLDao() {
+
+    }
 }
