208b2eaef7da5498b2ca97dbe6aad4ed9bea2d20
barroodc <53627915+barroodc@users.noreply.github.com>
2022-02-25 00:21:44 -0500

    Started filling in dao classes.

--- src/main/java/com/chrisportfolio/schoolmanagementsystem/dao/jdbcmySQLImpl/ExamDao.java
+++ src/main/java/com/chrisportfolio/schoolmanagementsystem/dao/jdbcmySQLImpl/ExamDao.java
@@ -2,11 +2,27 @@ package com.chrisportfolio.schoolmanagementsystem.dao.jdbcmySQLImpl;
 
 import com.chrisportfolio.schoolmanagementsystem.dao.daointerfaces.IExamDao;
 import com.chrisportfolio.schoolmanagementsystem.model.Exam;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public class ExamDao extends AbstractMySQLDao<Exam> implements IExamDao {
+
+    private static final Logger LOGGER = LogManager.getLogger(ExamDao.class);
+
+    private static final String INSERT = "INSERT INTO exam (exam_id, exam_type_id, subjects, " +
+            "marks, grades, term, class_name) VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+    private static final String GET_ONE = "SELECT exam_id, exam_type_id, subjects," +
+            "marks, grades, term, class_name FROM exam WHERE exam_id = ?";
+
+    private static final String UPDATE = "UPDATE exam SET exam_type_id = ?, subjects = ?, marks = ?, " +
+            "grades = ?, term = ? FROM exam WHERE exam_id = ?";
+
+    private static final String DELETE = "DELETE FROM exam WHERE exam_id = ?";
+
     @Override
     public Exam findByID(long id) throws SQLException {
         return null;
--- src/main/java/com/chrisportfolio/schoolmanagementsystem/dao/jdbcmySQLImpl/ExamResultDao.java
+++ src/main/java/com/chrisportfolio/schoolmanagementsystem/dao/jdbcmySQLImpl/ExamResultDao.java
@@ -2,11 +2,27 @@ package com.chrisportfolio.schoolmanagementsystem.dao.jdbcmySQLImpl;
 
 import com.chrisportfolio.schoolmanagementsystem.dao.daointerfaces.IExamResultDao;
 import com.chrisportfolio.schoolmanagementsystem.model.ExamResult;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public class ExamResultDao extends AbstractMySQLDao<ExamResult> implements IExamResultDao {
+
+    private static final Logger LOGGER = LogManager.getLogger(ClassroomDao.class);
+
+    private static final String INSERT = "INSERT INTO exam_result (exam_result_id, exam_id, course_id, " +
+            "student_id, mark, grade) VALUES (?, ?, ?, ?, ?, ?)";
+
+    private static final String GET_ONE = "SELECT exam_result_id, exam_id, course_id," +
+            "student_id, mark, grade FROM exam_result WHERE exam_result_id = ?";
+
+    private static final String UPDATE = "UPDATE exam_result SET exam_id = ?, course_id = ?, student_id = ?, " +
+            "mark = ?, grade = ? FROM exam_result WHERE exam_result_id = ?";
+
+    private static final String DELETE = "DELETE FROM exam_result WHERE exam_result_id = ?";
+
     @Override
     public ExamResult findByID(long id) throws SQLException {
         return null;
